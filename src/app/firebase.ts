a90f934181cb2ef8aacefb1b784304251ad9ead5
Daniel Soh <daniel.soh.2017@sis.smu.edu.sg>
2019-12-17 23:41:51 +0800

    first build with auth completed

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyCBmSSM3-EhDcfI7TvX8LHx83JOJ2mmlp0",
+    authDomain: "trucksy2020-54017.firebaseapp.com",
+    databaseURL: "https://trucksy2020-54017.firebaseio.com",
+    projectId: "trucksy2020-54017",
+    storageBucket: "trucksy2020-54017.appspot.com",
+    messagingSenderId: "544190166206",
+    appId: "1:544190166206:web:f07d772e4a069b16a31aef",
+    measurementId: "G-JFPWVV6WJ1"
+  }
+
+export default config

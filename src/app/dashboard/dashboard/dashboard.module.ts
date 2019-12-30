dd8bfc0751715044630f9a68fe5edc96f65985fa
Daniel Soh <daniel.soh.2017@sis.smu.edu.sg>
2019-12-30 17:35:46 +0800

    Refactor dashboard UI

--- src/app/dashboard/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard/dashboard.module.ts
@@ -6,16 +6,18 @@ import { FormsModule } from '@angular/forms';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {ShareModule} from "../../share.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    NbCardModule,
-    ThemeModule,
-    NgbModule,
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        NbCardModule,
+        ThemeModule,
+        NgbModule,
+        ShareModule,
+    ],
   declarations: [
     DashboardComponent,
   ],

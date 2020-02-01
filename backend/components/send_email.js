e3b6ee09da9c7258e985e2f93a68d5f2a3d9123c
smallsizeperson <mogawa.2016@economics.smu.edu.sg>
2020-02-01 02:20:32 +0000

    Changed 'from' email address to fix bug

--- backend/components/send_email.js
+++ backend/components/send_email.js
@@ -7,7 +7,7 @@ module.exports = {
     sendEmail: function (email, orderId, dateTimeOfPickup, pickUpAddress, dropOffAddress) {
         const msg = {
             to: `${email}`,
-            from: 'Rentalorry <trucksy2020@gmail.com>',
+            from: 'Rentalorry <masayoshi0ogawa@gmail.com>',
             subject: `Deliver Confirmation [Order: ${orderId}]`,
             text: `Your delivery from ${pickUpAddress} to ${dropOffAddress} at pickup ${dateTimeOfPickup} has been confirmed.`,
         };
